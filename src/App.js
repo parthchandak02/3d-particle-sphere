1f4244df3efbe54aa0d647b08eea305b8bb3316e
Parth Chandak <parth.chandak02@gmail.com>
2024-07-04 00:30:39 -0700

    Update for GitHub Pages deployment

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', backgroundColor: 'transparent' }}>
+      <Spline scene="https://prod.spline.design/Klk6kydm2OqssauT/scene.splinecode" />
+    </div>
+  );
+}
